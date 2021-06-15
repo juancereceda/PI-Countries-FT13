8ef3d0f94546467d2f9219443211745d3807c3b9
juancereceda <juanmcereceda@gmail.com>
2021-06-15 12:09:05 -0300

    code improvement

--- api/index.js
+++ api/index.js
@@ -1,5 +1,23 @@
 const server = require("./src/app.js");
-const { conn } = require("./src/db.js");
+const fetch = require("node-fetch");
+const { conn, Country } = require("./src/db.js");
+
+fetch("https://restcountries.eu/rest/v2/all")
+  .then((r) => r.json())
+  .then((r) => {
+    r.forEach((el) => {
+      let country = {
+        id: el.alpha3Code,
+        name: el.name,
+        flag: el.flag,
+        continent: el.region,
+        area: el.area,
+        population: el.population,
+      };
+      Country.create(country);
+    });
+  })
+  .catch((err) => console.log(err));
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,29 +1,10 @@
 const { Router } = require("express");
 const countries = require("./countries");
-const { Country } = require("../db");
 const activity = require("./activities");
-const fetch = require("node-fetch");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router();
-
-fetch("https://restcountries.eu/rest/v2/all")
-  .then((r) => r.json())
-  .then((r) => {
-    r.forEach((el) => {
-      let country = {
-        id: el.alpha3Code,
-        name: el.name,
-        flag: el.flag,
-        continent: el.region,
-        area: el.area,
-        population: el.population,
-      };
-      Country.create(country);
-    });
-  })
-  .catch((err) => console.log(err));
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
