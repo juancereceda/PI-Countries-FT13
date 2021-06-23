67b6dc2a831c38d8eca5dbe5180665287516e8e9
juancereceda <juanmcereceda@gmail.com>
2021-06-22 22:46:20 -0300

    solving

---
+++ src/routes/activities.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const { Op } = require("sequelize");
+const server = express();
+server.use(express.json());
+const { Activity, Country } = require("../db");
+
+server.get("/", async (req, res) => {
+  try {
+    const activities = await Activity.findAll();
+    return res.json(activities);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+server.put("/", async (req, res) => {
+  try {
+    console.log("here");
+    const { name, difficulty, duration, season, countries } = req.body;
+    const activity = await Activity.findOne({
+      where: {
+        name: {
+          [Op.eq]: name,
+        },
+      },
+    });
+    activity.difficulty = difficulty;
+    activity.duration = duration;
+    activity.season = season;
+    await activity.save();
+
+    let ctry;
+    if (Array.isArray(countries)) {
+      ctry = await Promise.all(
+        countries.map((country) => Country.findByPk(country))
+      );
+    }
+    if (ctry) {
+      await activity.setCountries(ctry);
+    }
+    return res.send(activity);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+server.post("/", async (req, res) => {
+  const { name, difficulty, duration, season, countries } = req.body;
+  const activity = await Activity.create({
+    name,
+    difficulty,
+    duration,
+    season,
+  });
+  let ctry;
+  if (Array.isArray(countries)) {
+    ctry = await Promise.all(
+      countries.map((country) => Country.findByPk(country))
+    );
+  }
+  if (ctry) {
+    await activity.setCountries(ctry);
+  }
+  return res.send(activity);
+});
+
+module.exports = server;
---
+++ src/routes/countries.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const server = express();
+const { Country, Activity } = require("../db");
+const { Op } = require("sequelize");
+const axios = require("axios");
+
+server.use(express.json());
+
+server.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await axios.get(
+      `https://restcountries.eu/rest/v2/alpha/${id}`
+    );
+    let foundById = await Country.findByPk(id.toUpperCase(), {
+      include: Activity,
+    });
+    const dataValues = {
+      nativename: response.data.nativeName,
+      capital: response.data.capital,
+      subregion: response.data.subregion,
+      language: response.data.languages[0].name,
+    };
+    foundById
+      ? res.send({ ...foundById.dataValues, ...dataValues })
+      : res.sendStatus(404);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+server.get("/", async (req, res) => {
+  const { name, continent } = req.query;
+  try {
+    if (name || continent) {
+      const filter = { where: {} };
+      name ? (filter.where.name = { [Op.iLike]: `%${name}%` }) : null;
+      continent ? (filter.where.continent = { [Op.eq]: continent }) : null;
+      const filteredByProps = await Country.findAll({
+        include: [{ model: Activity, require: true }],
+        ...filter,
+      });
+      filteredByProps.length < 1
+        ? res.send({ error: "No country was found" })
+        : res.send(filteredByProps);
+    } else {
+      const countries = await Country.findAll({
+        include: [{ model: Activity, require: true }],
+      });
+      res.send(countries);
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+module.exports = server;
