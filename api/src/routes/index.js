357cb711b2156d0b98710e86fba441fa7fbd92f6
juancereceda <juanmcereceda@gmail.com>
2021-07-01 22:05:53 -0300

    comment deleted

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,6 @@
 const { Router } = require("express");
 const countries = require("./countries");
 const activity = require("./activities");
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router();
 
